242005a7820d30fca316c29337b0b46c44d44765
JordanDonguy <jordan.donguy@gmail.com>
2026-03-26 20:53:23 +0100

    refactor(db): simplify Prisma client to direct export

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,18 +2,7 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client.ts";
 import { config } from "../lib/server/config.ts";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined };
-
-export function getPrisma() {
-  if (!globalForPrisma.prisma) {
-    const adapter = new PrismaPg({
-      connectionString: config.db.url,
-    });
-
-    globalForPrisma.prisma = new PrismaClient({ adapter });
-  }
-
-  return globalForPrisma.prisma;
-}
+const adapter = new PrismaPg({ connectionString: config.db.url });
+export const prisma = new PrismaClient({ adapter });
 
 export * from "../generated/prisma/client.ts";
